682e16c62bacd19cd11637bc638d0da924e580c0
GuyTuval <guytuval@gmail.com>
2020-04-15 20:06:18 +0300

    Updated according to changed file name.

--- src/app/core/services/snackbar-service.service.spec.ts
+++ src/app/core/services/snackbar-service.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SnackbarServiceService } from './snackbar-service.service';
+import { SnackbarService } from './snackbar-service.service';
 
 describe('SnackbarServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: SnackbarServiceService = TestBed.get(SnackbarServiceService);
+    const service: SnackbarService = TestBed.get(SnackbarService);
     expect(service).toBeTruthy();
   });
 });
